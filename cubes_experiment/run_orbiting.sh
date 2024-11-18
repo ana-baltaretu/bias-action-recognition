b05a5e6538867c2abf9f8e90b96225ec18cad22d
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2024-11-18 11:06:00 +0100

    Run orbiting

--- cubes_experiment/run_orbiting.sh
+++ cubes_experiment/run_orbiting.sh
@@ -1,16 +1,18 @@
 start_time=$(date +%s)
 
-animations=2
-cubes=1
-red=0
+animations=10
+max_cubes=6
 
 ### Actual script vvvvvvvv
-for i in $(seq 1 $animations); do
-    blender --background --python red_blue_cubes_orbiting_around_z.py -- --number $cubes \
-    --red $red --save "./animation_output/test_parallel/orbiting_${cubes}cubes_${red}red_$i" &
+for cubes in $(seq 1 $max_cubes); do
+  for red in $(seq 0 "$cubes"); do
+    for i in $(seq 1 $animations); do
+        blender --background --python red_blue_cubes_orbiting_around_z.py -- --number "$cubes" \
+        --red "$red" --save "./animation_output/actual_output/orbiting/orbiting_${cubes}C${red}R${cubes-red}B_pos$i" &
+    done
+    wait
+  done
 done
-
-wait
 ### Actual script ^^^^^^^^
 
 
