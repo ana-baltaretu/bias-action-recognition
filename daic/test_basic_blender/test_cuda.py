b614b8a3f6c8d73f69b2c6facc927e552f57608f
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2025-01-17 11:30:10 +0100

    Change cuda

--- daic/test_basic_blender/test_cuda.py
+++ daic/test_basic_blender/test_cuda.py
@@ -4,10 +4,11 @@ import bpy
 # Enable CUDA or OptiX
 bpy.context.preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
 
-# Ensure all GPUs are enabled
-for device in bpy.context.preferences.addons['cycles'].preferences.get_devices():
-    if device.type == 'CUDA' or device.type == 'OPTIX':
-        device.use = True
+
+gpus = bpy.context.preferences.addons['cycles'].preferences.get_devices()
+print(gpus)
+
+print(gpus[0])
 
 # Set Cycles to use GPU rendering
 bpy.context.scene.cycles.device = 'GPU'
