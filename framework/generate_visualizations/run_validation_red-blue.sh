b7867c7527eea3cb286a0d0c497c04ecc570f83b
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2025-02-03 15:19:55 +0100

    Move visualizations code

---
+++ framework/generate_visualizations/run_validation_red-blue.sh
@@ -0,0 +1,39 @@
+python test_on_video.py  --video_path data/cubes/bouncing/bouncing_1C0R1B_pos9.mp4 --output_file bouncing_1C0R1B_pos9 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/bouncing/bouncing_1C1R0B_pos4.mp4 --output_file bouncing_1C1R0B_pos4 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/bouncing/bouncing_2C0R2B_pos8.mp4 --output_file bouncing_2C0R2B_pos8 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/bouncing/bouncing_2C0R2B_pos9.mp4 --output_file bouncing_2C0R2B_pos9 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/bouncing/bouncing_2C2R0B_pos0.mp4 --output_file bouncing_2C2R0B_pos0 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_1C0R1B_pos8.mp4 --output_file orbiting_1C0R1B_pos8 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_1C1R0B_pos8.mp4 --output_file orbiting_1C1R0B_pos8 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C1R1B_pos4.mp4 --output_file orbiting_2C1R1B_pos4 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C1R1B_pos5.mp4 --output_file orbiting_2C1R1B_pos5 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C2R0B_pos0.mp4 --output_file orbiting_2C2R0B_pos0 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+
+
+
+
+
+
+
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_1C0R1G0B_pos0.mp4 --output_file orbiting_1C0R1G0B_pos0 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_1C0R1G0B_pos4.mp4 --output_file orbiting_1C0R1G0B_pos4 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C0R1G1B_pos8.mp4 --output_file orbiting_2C0R1G1B_pos8 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C0R2G0B_pos1.mp4 --output_file orbiting_2C0R2G0B_pos1 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+python test_on_video.py  --video_path data/cubes/orbiting/orbiting_2C1R1G0B_pos4.mp4 --output_file orbiting_2C1R1G0B_pos4 --checkpoint_model model_checkpoints/ConvLSTM_95.pth --dataset_path data/cubes-frames
+
+
+
+
+
+
+
