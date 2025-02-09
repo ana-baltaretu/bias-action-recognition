2e0e6b036765c2e474473cbed3b02137e4a3d329
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2025-02-09 14:33:34 +0100

    Update folder bindings

--- cubes_experiment/pre_processing_videos.sh
+++ cubes_experiment/pre_processing_videos.sh
@@ -8,8 +8,11 @@ TRAIN_PERCENTAGE=80
 # Define Apptainer image
 CONTAINER="pre_processing.sif"
 
+# Bind FOLDER_PATH so the container can access it
+BIND="--bind $FOLDER_PATH:$FOLDER_PATH"
+
 # Run processing steps inside the Apptainer container
-apptainer exec $CONTAINER python3 move_green_videos.py "$FOLDER_PATH"
-apptainer exec $CONTAINER python3 generate_labels_for_training.py "$FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
-apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/train-validation"
-apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/test"
+apptainer exec "$BIND" $CONTAINER python3 move_green_videos.py "$FOLDER_PATH"
+apptainer exec "$BIND" $CONTAINER python3 generate_labels_for_training.py "$FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
+apptainer exec "$BIND" $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/train-validation"
+apptainer exec "$BIND" $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/test"
