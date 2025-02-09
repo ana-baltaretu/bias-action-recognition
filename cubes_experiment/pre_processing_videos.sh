0b31b69f17a2b6149546620f82e8e4228d1aeb1e
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2025-02-09 14:17:13 +0100

    Add image for pre-processing frames

--- cubes_experiment/pre_processing_videos.sh
+++ cubes_experiment/pre_processing_videos.sh
@@ -1,11 +1,15 @@
-FOLDER_PATH="/tudelft.net/staff-umbrella/StudentsCVlab/abaltaretu/bias-action-recognition/daic/ConvLSTM_model_training/input_data" # TODO: Update with the actual folder path
+#!/bin/bash
 
-python move_green_videos.py "$FOLDER_PATH"
-
+# Define paths
+FOLDER_PATH="/tudelft.net/staff-umbrella/StudentsCVlab/abaltaretu/bias-action-recognition/daic/ConvLSTM_model_training/input_data"
 MODEL_PATH="/tudelft.net/staff-umbrella/StudentsCVlab/abaltaretu/bias-action-recognition/daic/ConvLSTM_model_training/action-recognition-by-eriklindernoren"
 TRAIN_PERCENTAGE=80
 
-python generate_labels_for_training.py "$FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
+# Define Apptainer image
+CONTAINER="pre_processing.sif"
 
-python extract_frames.py --dataset_path "{$FOLDER_PATH}/train-validation" # TODO: Get this working
-python extract_frames.py --dataset_path "{$FOLDER_PATH}/test" # TODO: Get this working
+# Run processing steps inside the Apptainer container
+apptainer exec $CONTAINER python3 move_green_videos.py "$FOLDER_PATH"
+apptainer exec $CONTAINER python3 generate_labels_for_training.py "$FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
+apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/train-validation"
+apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/test"
