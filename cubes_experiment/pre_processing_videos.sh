cece2a06826fa4d1fb1c1e6d65f03e15bbf5186e
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2025-02-09 14:48:25 +0100

    Put things in tmp folder

--- cubes_experiment/pre_processing_videos.sh
+++ cubes_experiment/pre_processing_videos.sh
@@ -5,14 +5,22 @@ FOLDER_PATH="../daic/ConvLSTM_model_training/input_data/"
 MODEL_PATH="../daic/ConvLSTM_model_training/action-recognition-by-eriklindernoren"
 TRAIN_PERCENTAGE=80
 
+TEMP_FOLDER_PATH="/tmp/data_ConvLSTM/"
+TEMP_MODEL_PATH="/tmp/model_ConvLSTM/"
+
+# Copy to tmp folder cause apptainer can't see normal folder for some reason???
+cp -r "$FOLDER_PATH" "$TEMP_FOLDER_PATH"
+cp -r "$MODEL_PATH" "$TEMP_MODEL_PATH"
+
 # Define Apptainer image
 CONTAINER="pre_processing.sif"
 
-# Bind FOLDER_PATH so the container can access it
-BIND="--bind $FOLDER_PATH:$FOLDER_PATH"
-
 # Run processing steps inside the Apptainer container
-apptainer exec "$BIND" $CONTAINER python3 move_green_videos.py "$FOLDER_PATH"
-apptainer exec "$BIND" $CONTAINER python3 generate_labels_for_training.py "$FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
-apptainer exec "$BIND" $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/train-validation"
-apptainer exec "$BIND" $CONTAINER python3 extract_frames.py --dataset_path "$FOLDER_PATH/test"
+apptainer exec $CONTAINER python3 move_green_videos.py "$TEMP_FOLDER_PATH"
+apptainer exec $CONTAINER python3 generate_labels_for_training.py "$TEMP_FOLDER_PATH" "$MODEL_PATH" "$TRAIN_PERCENTAGE"
+apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$TEMP_FOLDER_PATH/train-validation"
+apptainer exec $CONTAINER python3 extract_frames.py --dataset_path "$TEMP_FOLDER_PATH/test"
+
+# Move back to normal folder
+cp -r "$TEMP_FOLDER_PATH" "$FOLDER_PATH"
+cp -r "$TEMP_MODEL_PATH" "$MODEL_PATH"
